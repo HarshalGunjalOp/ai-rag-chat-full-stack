c5a48dba5284e9aa8b0ed125acc8a01b60f86db9
HarshalGunjalOp <hrshlgunjal3@gmail.com>
2025-06-22 05:16:28 +0530

    Fixed the class error

--- frontend/src/components/Messages/MessageBubble.tsx
+++ frontend/src/components/Messages/MessageBubble.tsx
@@ -60,7 +60,11 @@ export default function MessageBubble({ msg }: { msg: { id: string; type: 'user'
           "text-xs opacity-60 mt-2",
           isUser ? "text-right" : "text-left"
         )}>
-          {new Date(msg.createdAt).toLocaleTimeString()}
+          {new Date(msg.createdAt).toLocaleTimeString([], {
+            hour: '2-digit',
+            minute: '2-digit',
+          })
+          }
         </div>
       </div>
 
@@ -68,8 +72,8 @@ export default function MessageBubble({ msg }: { msg: { id: string; type: 'user'
       {isUser && (
         <div className="flex-shrink-0 w-8 h-8 bg-blue-500 rounded-full flex items-center justify-center">
           <span className="text-white text-sm font-medium">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
             </svg>
 
           </span>
